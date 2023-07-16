3ca5d29ae1bc35ec8ed053b8af04400c50dcf75c
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-07-16 18:01:03 +0100

    Create image.py and remove video utilities

--- daily_phrase/image.py
+++ daily_phrase/image.py
@@ -1,28 +1,9 @@
 import requests
 from pathlib import Path
 import os
-from moviepy.editor import ImageClip, TextClip, CompositeVideoClip
 
 
 API_KEY = os.environ.get("UNSPLASH_API_KEY")
-
-
-def _create_video_from_image(video_length: float, tmp_dir: Path) -> None:
-    image_path = next(tmp_dir.rglob("*.jpg"))  # Get the first jpg image in tmp/
-    clip = ImageClip(str(image_path), duration=video_length)
-    clip_resized = clip.resize(height=1920, width=1080)
-    clip_resized.write_videofile(tmp_dir / "video.mp4", codec='libx264')
-
-
-def _add_text_overlay(video_path: Path, text: str, start_time: float) -> None:
-    video = ImageClip(str(video_path))
-    txt_clip = (TextClip(text, fontsize=70, color='white')
-                .set_position(('center', 'bottom'))
-                .set_start(start_time)
-                .set_duration(video.duration - start_time))
-
-    result = CompositeVideoClip([video, txt_clip])
-    result.write_videofile(f"{video_path.parent}/{video_path.stem}_overlay.mp4", codec='libx264')
 
 
 def _fetch_random_image_from_unsplash(country: str, tmp_dir: Path) -> None:
