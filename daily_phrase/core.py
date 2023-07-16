3a67226a390971b890dbeb43348909e2d26252ed
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-07-16 23:14:03 +0100

    Add main execution path of program in

--- daily_phrase/core.py
+++ daily_phrase/core.py
@@ -2,7 +2,35 @@
 from pathlib import Path
 from moviepy.editor import ImageClip, TextClip, CompositeVideoClip
 
-from typing import List, Dict, Any
+from audio import AudioPhrase
+
+
+from image import download_random_image_from_unsplash
+from tempfile import TemporaryDirectory
+
+
+def main(country: str) -> None:
+    """Main entry point for the daily_phrase package.
+    """
+    audio_phrases = _load_phrases()  # TODO: implement this function
+    with TemporaryDirectory() as tmp_dir:
+        tmp_dir = Path(tmp_dir)
+        _create_audio_files(tmp_dir, audio_phrases)
+        download_random_image_from_unsplash(country, tmp_dir)
+        _create_video_from_image(video_length=5.0, tmp_dir=tmp_dir)
+
+
+def _load_phrases() -> list[AudioPhrase]:
+    pass
+
+
+def _create_audio_files(tmp_path: Path, audio_phrases: list[AudioPhrase]) -> None:
+    """Process a list of AudioPhrase instances by preprocessing their prompts and
+    creating their audio files.
+    """
+    for audio_phrase in audio_phrases:
+        audio_phrase.preprocess_prompts()
+        audio_phrase.create_audio(tmp_path)
 
 
 def _create_video_from_image(video_length: float, tmp_dir: Path) -> None:
@@ -12,7 +40,7 @@ def _create_video_from_image(video_length: float, tmp_dir: Path) -> None:
     clip_resized.write_videofile(tmp_dir / "video.mp4", codec='libx264')
 
 
-def _add_audio_to_video(phrases: List[Dict[str, Any]], video_path: Path) -> None:
+def _add_audio_to_video(phrases: list[AudioPhrase], video_path: Path) -> None:
     pass
 
 
