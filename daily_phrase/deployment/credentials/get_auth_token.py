fb0e128c48d9372e47d2daefdeafadc492d44d85
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-08-27 20:47:42 +0100

    Lint

--- daily_phrase/deployment/credentials/get_auth_token.py
+++ daily_phrase/deployment/credentials/get_auth_token.py
@@ -1,8 +1,11 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 # Load your client secrets
-flow = InstalledAppFlow.from_client_secrets_file('credentials/daily-phrase-secrets.json', ['https://www.googleapis.com/auth/youtube.upload'])
+flow = InstalledAppFlow.from_client_secrets_file(
+    "credentials/daily-phrase-secrets.json",
+    ["https://www.googleapis.com/auth/youtube.upload"],
+)
 
 # Run the OAuth 2.0 flow
 credentials = flow.run_local_server(port=8080)
@@ -16,4 +19,3 @@ if not credentials.valid:
 
 refresh_token = credentials.refresh_token
 print("Refresh Token:", refresh_token)
-
